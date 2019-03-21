9b5af1d08e32105d92ccf33e7e3042e2edcb6fa5
laralcq <laraloicq@gmail.com>
2019-03-21 09:29:13 +0100

    Updates

---
+++ precache-manifest.409f60d1523529b166c759bf56ade888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15fd8a1e9c0e9144ea7e",
+    "url": "/happy-drink-react/static/css/main.6dc5f38a.chunk.css"
+  },
+  {
+    "revision": "15fd8a1e9c0e9144ea7e",
+    "url": "/happy-drink-react/static/js/main.74f125e6.chunk.js"
+  },
+  {
+    "revision": "8ee8480b7a07c49a888d",
+    "url": "/happy-drink-react/static/js/runtime~main.fd44a4c8.js"
+  },
+  {
+    "revision": "157f51664789baf60b64",
+    "url": "/happy-drink-react/static/js/2.1279535a.chunk.js"
+  },
+  {
+    "revision": "234985988cbbee6dc009373aba5ac820",
+    "url": "/happy-drink-react/static/media/bottle.23498598.svg"
+  },
+  {
+    "revision": "f6239d64a7a7f03e8f20d34382d7d663",
+    "url": "/happy-drink-react/index.html"
+  }
+];
